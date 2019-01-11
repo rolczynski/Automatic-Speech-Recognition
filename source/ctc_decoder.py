d7435a3a3fc52d8461d6a57e31fd206d31a229a8
rafal <rafal.rolczynski@gmail.com>
2019-01-11 16:28:16 +0100

    Refactor ctc decoder to be more understandable

--- source/ctc_decoder.py
+++ source/ctc_decoder.py
@@ -1,121 +1,118 @@
-"""
-Author: Awni Hannun
-This is an example CTC decoder written in Python. The code is
-intended to be a simple example and is not designed to be
-especially efficient.
-The algorithm is a prefix beam search for a model trained
-with the CTC loss function.
-For more details checkout either of these references:
-  https://distill.pub/2017/ctc/#inference
-  https://arxiv.org/abs/1408.2873
-"""
-
 import numpy as np
+from typing import List
 import math
 import collections
+from source.text import Alphabet
 NEG_INF = -float("inf")
 
 
-def batch_decode(y_hat, alphabet):
-    """ Enable to batch decode using multiprocessing """
-    raise NotImplemented
+def batch_decode(batch_y_hat, alphabet, **kwargs):
+    """ Enable to batch decode (using multiprocessing in the future). """
+    return [decode(y_hat, alphabet, **kwargs) for y_hat in batch_y_hat]
 
 
-def __decode(probs, alphabet, beam_size=100, prune=0.001):
+def decode(probs: np.ndarray, alphabet: Alphabet, beam_size=100, prune=0.001):
     """
     Performs inference for the given output probabilities.
     Arguments:
         probs: The output probabilities (e.g. post-softmax) for each
           time step. Should be an array of shape (time x output dim).
         beam_size (int): Size of the beam to use during inference.
-        blank (int): Index of the CTC blank label.
+
     Returns the output label sequence and the corresponding negative
     log-likelihood estimated by the decoder.
+
+    For more details checkout either of these references:
+        https://distill.pub/2017/ctc/#inference
+        https://arxiv.org/abs/1408.2873
     """
-    # Add the blank token to the alphabet
-    alphabet._label_to_str.append('')
-    alphabet._str_to_label[''] = 35
-
-    T, S = probs.shape
+    Time, _ = probs.shape
     probs, prune = np.log(probs), np.log(prune)
-
-    # Elements in the beam are (prefix, (p_blank, p_no_blank))
-    # Initialize the beam with the empty sequence, a probability of
-    # 1 for ending in blank and zero for ending in non-blank
-    # (in log space).
     beam = [('', (0.0, NEG_INF))]
 
-    for t in range(T):  # Loop over time
+    for time in range(Time):
+        next_beam = make_beam()
 
-        # A default dictionary to store the next step candidates.
-        next_beam = __make_new_beam()
+        pruned_indices, = np.where(probs[time] > prune)
+        probable_characters = [(i, alphabet.string_from_label(i)) for i in pruned_indices]
 
-        pruned_indices, = np.where(probs[t] > prune)
-        pruned_alphabet = [(i, alphabet.string_from_label(i))
-                           for i in pruned_indices]
+        for char_index, char in probable_characters:
+            char_prob = probs[time, char_index]
 
-        for i, s in pruned_alphabet:  # Loop over most probable characters
-            P = probs[t, i]
+            for prefix, (P_b, P_nb) in beam:
+                update_next_beam(next_beam, prefix, char, char_prob, [P_b, P_nb])
 
-            # The variables P_b and P_nb are respectively the
-            # probabilities for the prefix given that it ends in a
-            # blank and does not end in a blank at this time step.
-            for prefix, (P_b, P_nb) in beam:  # Loop over beam
-
-                # If we propose a blank the prefix doesn't change.
-                # Only the probability of ending in blank gets updated.
-                if s == '':
-                    next_P_b, next_P_nb = next_beam[prefix]
-                    next_P_b = __logsumexp(next_P_b, P_b + P, P_nb + P)
-                    next_beam[prefix] = (next_P_b, next_P_nb)
-                    continue
-
-                # Extend the prefix by the new character s and add it to
-                # the beam. Only the probability of not ending in blank
-                # gets updated.
-                end_t = prefix[-1] if prefix else None
-                next_prefix = prefix + s
-                next_P_b, next_P_nb = next_beam[next_prefix]
-                if s != end_t:
-                    next_P_nb = __logsumexp(next_P_nb, P_b + P, P_nb + P)
-                else:
-                    # We don't include the previous probability of not ending
-                    # in blank (P_nb) if s is repeated at the end. The CTC
-                    # algorithm merges characters not separated by a blank.
-                    next_P_nb = __logsumexp(next_P_nb, P_b + P)
-
-                # *NB* this would be a good place to include an LM score.
-                next_beam[next_prefix] = (next_P_b, next_P_nb)
-
-                # If s is repeated at the end we also update the unchanged
-                # prefix. This is the merging case.
-                if s == end_t:
-                    next_P_b, next_P_nb = next_beam[prefix]
-                    next_P_nb = __logsumexp(next_P_nb, P_nb + P)
-                    next_beam[prefix] = (next_P_b, next_P_nb)
-
-        # Sort and trim the beam before moving on to the
-        # next time-step.
-        beam = sorted(next_beam.items(),
-                      key=lambda x: __logsumexp(*x[1]),
-                      reverse=True)
-        beam = beam[:beam_size]
+        beam = choose_best_candidates(next_beam, n=beam_size)
+        # rescore(beam)
 
     best = beam[0]
-    return best[0], -__logsumexp(*best[1])
+    return best[0], -logsum(*best[1])
 
 
-def __make_new_beam():
-    fn = lambda: (NEG_INF, NEG_INF)
-    return collections.defaultdict(fn)
+def update_next_beam(next_beam: collections.defaultdict,
+                     prefix: str, char: str, char_prob: int, probs: List):
+    """ Implementation CTC logic for calculation prefix probabilities. """
+    P, [P_b, P_nb] = char_prob, probs
+    prefix_end = prefix[-1] if prefix else None
+    next_prefix = prefix + char
+
+    if char == '':
+        # If we propose a blank the prefix doesn't change.
+        # Only the probability of ending in blank gets updated.
+        next_P_b, next_P_nb = next_beam[prefix]
+        next_P_b = logsum(next_P_b, P_b + P, P_nb + P)
+        next_beam[prefix] = (next_P_b, next_P_nb)
+        return
+
+    elif char == prefix_end:
+        # We don't include the previous probability of not ending
+        # in blank (P_nb) if s is repeated at the end. The CTC
+        # algorithm merges characters not separated by a blank.
+        next_P_b, next_P_nb = next_beam[next_prefix]
+        next_P_nb = logsum(next_P_nb, P_b + P)
+        next_beam[next_prefix] = (next_P_b, next_P_nb)
+
+        # Also update the unchanged prefix. This is the merging case.
+        next_P_b, next_P_nb = next_beam[prefix]
+        next_P_nb = logsum(next_P_nb, P_nb + P)
+        next_beam[prefix] = (next_P_b, next_P_nb)
+        return
+
+    else:
+        # Extend the prefix by the new character s and add it to
+        # the beam. Only the probability of not ending in blank
+        # gets updated.
+        next_P_b, next_P_nb = next_beam[next_prefix]
+        next_P_nb = logsum(next_P_nb, P_b + P, P_nb + P)
+        next_beam[next_prefix] = (next_P_b, next_P_nb)
+        return
 
 
-def __logsumexp(*args):
-    """
-    Stable log sum exp.
-    """
+def choose_best_candidates(beam: collections.defaultdict, n: int):
+    """ Sort and trim the beam before moving on to the next search."""
+    next_beam = sorted(beam.items(), key=lambda x: logsum(*x[1]), reverse=True)
+    return next_beam[:n]
+
+
+def make_beam():
+    """ A default dictionary to store the next step candidates. """
+    return collections.defaultdict(lambda: (NEG_INF, NEG_INF))
+
+
+def logsum(*args):
+    """ Compute log sum using exp trick. """
     if all(a == NEG_INF for a in args):
         return NEG_INF
     a_max = max(args)
     lsp = math.log(sum(math.exp(a - a_max) for a in args))
     return a_max + lsp
+
+
+def rescore(beam: collections.defaultdict):
+    """
+    Take a use of misspelling rules (the confusion matrix) to put the performance up.
+
+    References:
+        - https://github.com/barrust/pyspellchecker
+    """
+    raise NotImplemented
