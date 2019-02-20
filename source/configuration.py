06980ebd615ef6f77e69bfe13d60f7e8ad4c0407
rafal <rafal.rolczynski@gmail.com>
2019-02-20 19:30:46 +0100

    Add new attributes to configuration file

--- source/configuration.py
+++ source/configuration.py
@@ -11,11 +11,13 @@ class Configuration:
         """ All parameters saved in .yaml file convert to dot accessible """
         self._file_path = file_path
         self._data = self._read_yaml_file()
-        self._check_file(required_keys=['alphabet', 'model', 'callbacks', 'optimizer'])
+        self._check_file(required_keys=['alphabet', 'features_extractor', 'model', 'callbacks', 'optimizer', 'decoder'])
         self.alphabet = self._data.get('alphabet')
+        self.features_extractor = self._data.get('features_extractor')
         self.model = self._data.get('model')
         self.callbacks = self._data.get('callbacks')
         self.optimizer = self._data.get('optimizer')
+        self.decoder = self._data.get('decoder')
 
 
     def _read_yaml_file(self):
