61250b7eec51050d24b698c9b6fc5077cff0df68
rafal <rafal.rolczynski@gmail.com>
2019-02-20 20:34:25 +0100

    Delete unnecessary utils

--- source/utils.py
+++ source/utils.py
@@ -3,39 +3,10 @@ import os
 import logging
 
 
-def make_keras_picklable():
-    from tempfile import NamedTemporaryFile as temp
-    from keras.engine.saving import save_model, load_model as load_keras_model
-    from keras.models import Model
-
-    def __getstate__(self):
-        with temp(suffix='.hdf5', delete=True) as f:
-            save_model(self, f.name, overwrite=True)
-            model_str = f.read()
-        return {'model_str': model_str}
-
-    def __setstate__(self, state):
-        with temp(suffix='.hdf5', delete=True) as fd:
-            fd.write(state['model_str'])
-            fd.flush()
-            model = load_keras_model(fd.name)
-        self.__dict__ = model.__dict__
-
-    Model.__getstate__ = __getstate__
-    Model.__setstate__ = __setstate__
-
-
 def save(data, file_name):
     """ Save arbitrary data in the file. """
     with open(file_name, mode='wb') as file:
         dill.dump(data, file)
-
-
-def load_model(model_path):
-    """ Load model even without the deepspeech package. """
-    with open(model_path, mode='rb') as f:
-        deepspeech = dill.load(f)
-    return deepspeech
 
 
 def chdir(to='ROOT'):
