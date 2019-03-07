3e0c275129f5eab48be84b33af404d78477d6ef9
rafal <rafal.rolczynski@gmail.com>
2019-03-07 20:23:49 +0100

    Add random state to model definition

--- tests/test_deepspeech.py
+++ tests/test_deepspeech.py
@@ -1,61 +1,63 @@
 import os
+import shutil
 import pytest
 import numpy as np
 import warnings
 warnings.simplefilter("ignore", category=DeprecationWarning)    # Tensorflow warnings
-
-from deepspeech import DeepSpeech
+from typing import List
+from keras.engine.training import Model
+from deepspeech import DeepSpeech, Configuration, FeaturesExtractor, Alphabet, DataGenerator, History
 
 
 @pytest.fixture
-def config_path():
+def config_path() -> str:
     return 'test_model/configuration.yaml'
 
 
 @pytest.fixture
-def alphabet_path():
+def alphabet_path() -> str:
     return 'test_model/alphabet.txt'
 
 
 @pytest.fixture
-def config(config_path):
+def config(config_path) -> Configuration:
     return DeepSpeech.get_configuration(config_path)
 
 
 @pytest.fixture
-def alphabet(alphabet_path):
+def alphabet(alphabet_path: str) -> Alphabet:
     return DeepSpeech.get_alphabet(alphabet_path)
 
 
-def test_config(config):
+def test_config(config: Configuration):
     required_attrs = ['features_extractor', 'model', 'callbacks', 'optimizer', 'decoder']
     assert all(hasattr(config, attr) for attr in required_attrs)
 
 
-def test_get_model(config):
-    from keras.engine.training import Model
+def test_get_model(config: Configuration):
     model = DeepSpeech.get_model(**config.model, is_gpu=False)
     assert type(model) == Model
+    new_model = DeepSpeech.get_model(**config.model, is_gpu=False)
+    assert is_same(model.get_weights(), new_model.get_weights())    # Test random seed
 
 
-def test_get_features_extractor(config):
-    from deepspeech import FeaturesExtractor
+def test_get_features_extractor(config: Configuration):
     features_extractor = DeepSpeech.get_features_extractor(**config.features_extractor)
     assert type(features_extractor) == FeaturesExtractor
 
 
-def test_get_decoder(config, alphabet):
+def test_get_decoder(config: Configuration, alphabet: Alphabet):
     model = DeepSpeech.get_model(**config.model, is_gpu=False)
     decoder = DeepSpeech.get_decoder(alphabet=alphabet, model=model, **config.decoder)
     assert callable(decoder)
 
 
-def test_get_callbacks(config):
+def test_get_callbacks(config: Configuration):
     callbacks = DeepSpeech.get_callbacks(home_dir='test_model', configurations=config.callbacks)
     assert len(callbacks) == 6
 
 
-def test_compile_model(config):
+def test_compile_model(config: Configuration):
     model = DeepSpeech.get_model(**config.model, is_gpu=False)
     optimizer = DeepSpeech.get_optimizer(**config.optimizer)
     loss = DeepSpeech.get_loss()
@@ -64,21 +66,21 @@ def test_compile_model(config):
 
 
 @pytest.fixture
-def deepspeech(config_path, alphabet_path):
+def deepspeech(config_path: str, alphabet_path: str) -> DeepSpeech:
     return DeepSpeech.construct(config_path, alphabet_path)
 
 
 @pytest.fixture
-def audio_file_paths():
+def audio_file_paths() -> List[str]:
     return ['test_data/audio/0000.wav', 'test_data/audio/0001.wav']
 
 
-def test_get_features(deepspeech, audio_file_paths):
+def test_get_features(deepspeech: DeepSpeech, audio_file_paths: List[str]):
     features = deepspeech.get_features(audio_file_paths)
     assert features.shape == (2, 132, 26)
 
 
-def test_get_labels_and_get_transcripts(deepspeech):
+def test_get_labels_and_get_transcripts(deepspeech: DeepSpeech):
     bad_transcripts = ['to jest je$st!', 'test']
     correct_transcripts = ['to jest jest', 'test']
     labels = deepspeech.get_labels(bad_transcripts)
@@ -89,7 +91,7 @@ def test_get_labels_and_get_transcripts(deepspeech):
     assert transformed_transcripts == correct_transcripts
 
 
-def test_create_generator_from_audio_files(deepspeech):
+def test_create_generator_from_audio_files(deepspeech: DeepSpeech):
     generator = deepspeech.create_generator(file_path='test_data/audio.csv', source='from_audio_files', batch_size=5)
     assert len(generator) == 2
     X, y = generator[0]
@@ -97,7 +99,7 @@ def test_create_generator_from_audio_files(deepspeech):
     assert y.shape == (5, 44)
 
 
-def test_create_generator_from_prepared_features(deepspeech):
+def test_create_generator_from_prepared_features(deepspeech: DeepSpeech):
     generator = deepspeech.create_generator(file_path='test_data/features.hdf5', source='from_prepared_features', batch_size=5)
     assert len(generator) == 2
     X, y = generator[0]
@@ -106,13 +108,11 @@ def test_create_generator_from_prepared_features(deepspeech):
 
 
 @pytest.fixture
-def generator(deepspeech):
+def generator(deepspeech: DeepSpeech) -> DataGenerator:
     return deepspeech.create_generator(file_path='test_data/features.hdf5', source='from_prepared_features', batch_size=5)
 
 
-def test_fit(deepspeech, generator, config_path, alphabet_path):
-    from deepspeech import History
-
+def test_fit(deepspeech: DeepSpeech, generator: DataGenerator, config_path: str, alphabet_path: str):
     # Test save best weights (overwrite the best result)
     deepspeech.save('test_model/test_weights.hdf5')
     distributed_weights = deepspeech.distributed_model.get_weights()
@@ -145,35 +145,22 @@ def test_decode():
     pass
 
 
-def test_save_load(deepspeech):
-    is_same = lambda A, B: all(np.array_equal(a, b) for a, b in zip(A, B))
-
-    model_dir = 'test_model'
-    config_path = os.path.join(model_dir, 'configuration.yaml')
-    alphabet_path = os.path.join(model_dir, 'alphabet.txt')
-    weights_path = os.path.join(model_dir, 'weights.hdf5')
-
-    deepspeech_weights = deepspeech.model.get_weights()
+def test_save_load(deepspeech: DeepSpeech, config: Configuration, config_path: str, alphabet_path: str):
+    weights_path = os.path.join('test_model', 'weights.hdf5')
+    model_weights = deepspeech.model.get_weights()
     deepspeech.save(weights_path)
 
     new_deepspeech = DeepSpeech.construct(config_path, alphabet_path)
-    new_deepspeech_weights = new_deepspeech.model.get_weights()
-
-    assert not is_same(deepspeech_weights, new_deepspeech_weights)
+    new_deepspeech.model = deepspeech.get_model(**config.model, is_gpu=False, random_state=123)
+    new_model_weights = new_deepspeech.model.get_weights()
+    assert not is_same(model_weights, new_model_weights)
 
     new_deepspeech.load(weights_path)
-    new_deepspeech_weights = new_deepspeech.model.get_weights()
-    assert is_same(deepspeech_weights, new_deepspeech_weights)
+    new_model_weights = new_deepspeech.model.get_weights()
+    assert is_same(model_weights, new_model_weights)
 
 
-def test_load_pretrained_models(deepspeech):
-    deepspeech_weights = deepspeech.model.get_weights()
-    deepspeech.load('pl')
-    new_deepspeech_weights = deepspeech.model.get_weights()
-    assert not is_same(deepspeech_weights, new_deepspeech_weights)
-
-
-def test_call(deepspeech, audio_file_paths):
+def test_call(deepspeech: DeepSpeech, audio_file_paths: List[str]):
     # sentences = deepspeech(audio_file_paths)
     # assert len(sentences) == 2
     pass
@@ -192,7 +179,6 @@ def test_utils_load():
 
 def test_end():
     """ Clean the directory at the end. """
-    import os, shutil
     os.rename('test_model/alphabet.txt', 'alphabet.txt')
     os.rename('test_model/configuration.yaml', 'configuration.yaml')
     shutil.rmtree('test_model')
