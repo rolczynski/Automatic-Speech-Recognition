69bf65a3c3d0aac58bfd4f67115b4d5f95989161
rafal <rafal.rolczynski@gmail.com>
2019-07-16 17:31:51 +0200

    Adjust tests

--- tests/source/test_generator.py
+++ tests/source/test_generator.py
@@ -1,5 +1,8 @@
-from deepspeech import DataGenerator
-from utils import chdir
+from copy import deepcopy as copy
+
+from generator import DistributedDataGenerator
+from source.deepspeech import DataGenerator
+from source.utils import chdir
 
 chdir(to='ROOT')
 
@@ -9,3 +12,11 @@ def test_create_generator_from_audio_files(generator: DataGenerator):
     X, y = generator[0]
     assert X.shape == (2, 1477, 80)
     assert y.shape == (2, 206)
+
+
+def test_distributed_generator(generator: DataGenerator):
+    generator = DistributedDataGenerator(
+        generators=[copy(generator) for i in range(5)]
+    )
+    assert len(generator) == 10
+    assert generator[9]
