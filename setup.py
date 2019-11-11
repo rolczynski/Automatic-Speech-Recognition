cc45732cc3e983f35a7fb1ad3bc594e9ec6cb899
rolczynski <rafal.rolczynski@gmail.com>
2019-11-11 23:22:08 +0100

    Update setup file

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 with open("README.md", 'r') as f:
     long_description = f.read()
 
 setup(
-   name='deepspeech_keras',
+   name='automatic_speech_recognition',
    version='1.0',
-   description='Keras API on Deepspeech',
+   description='Keras API on Automatic Speech Recognition',
    long_description=long_description,
    license="GNU",
    author='Rolczynski Rafal',
    include_package_data=True,
-   packages=['deepspeech_keras'],
+   packages=['automatic_speech_recognition'],
+   install_requires=['h5py', 'numpy', 'pandas', 'scipy', 'python_speech_features', 'pytest', 'tensorflow',
+                     'testfixtures']
 )
