06c2044ef774832c23759645eee752c60894d905
rolczynski <rafal.rolczynski@gmail.com>
2020-01-02 14:15:10 +0100

    Adjust setup file

--- setup.py
+++ setup.py
@@ -4,14 +4,20 @@ with open("README.md", 'r') as f:
     long_description = f.read()
 
 setup(
-   name='automatic_speech_recognition',
+   name='automatic-speech-recognition',
    version='1.0',
-   description='Keras API on Automatic Speech Recognition',
+   url='https://github.com/rolczynski/Automatic-Speech-Recognition',
+   description='Distill the Automatic Speech Recognition in TensorFlow',
    long_description=long_description,
    license="GNU",
    author='Rolczynski Rafal',
+   author_email='rafal.rolczynski@gmail.com',
    include_package_data=True,
    packages=['automatic_speech_recognition'],
-   install_requires=['h5py', 'numpy', 'pandas', 'scipy', 'python_speech_features', 'pytest', 'tensorflow',
-                     'testfixtures']
+   install_requires=[
+      'tensorflow>=2.0', 'pandas', 'pytables',
+      'google-cloud-storage',    # Load weights
+      'python-speech-features>=0.6'
+   ],
+   python_requires='~=3.7',
 )
